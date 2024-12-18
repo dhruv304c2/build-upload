8260f3bd035005f77200ad44d50316a91034de98
dhruv304c2 <62604491+dhruv304c2@users.noreply.github.com>
2024-12-18 13:59:10 +0530

    chore: log text and use string de-serialization

--- src/services/diawi/feats/upload.rs
+++ src/services/diawi/feats/upload.rs
@@ -1,4 +1,5 @@
 use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::error::Error;
 use std::time::Duration;
@@ -7,6 +8,19 @@ use std::thread::sleep;
 pub struct UploadResponse{
     pub(crate) link: String,
     pub(crate) qr_code: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct JobResponse{
+    pub(crate) job : String
+}
+
+#[derive(Serialize,Deserialize)]
+struct StatusResponse{
+    pub(crate) status : usize,
+    pub(crate) message: Option<String>,
+    pub(crate) link: Option<String>,
+    pub(crate) qrcode: Option<String> 
 }
 
 pub fn upload(token : &String, file_path : &String) -> Result<UploadResponse, Box<dyn Error>>{
@@ -20,9 +34,11 @@ pub fn upload(token : &String, file_path : &String) -> Result<UploadResponse, Bo
         .form(&form)
         .send()?;
 
-    let upload_body : serde_json::Value = upload_response.json()?;
-    println!("diawi upload response: {}", upload_body);
-    let job_id = upload_body["job"].as_str().expect("could not get job id from diawi upload API response");
+    let upload_json = upload_response.text()?;
+    println!("diawi upload response: {}", upload_json);
+
+    let job : JobResponse = serde_json::from_str(&upload_json)?;
+    let job_id = job.job;
 
     println!("created upload job: {}", job_id);
 
@@ -33,24 +49,26 @@ pub fn upload(token : &String, file_path : &String) -> Result<UploadResponse, Bo
         pools += 1;
 
         let status_response = client.get(format!("https://upload.diawi.com/status?token={}&job={}", token, job_id)).send()?;
-        let status_body : serde_json::Value = status_response.json()?;
+        let status_json = status_response.text()?;
 
-        let status = status_body["status"].as_i64().expect("failed to get status form diawi status API response");
+        println!("status response: {}", status_json);
 
-        if status == 2001{
+        let status : StatusResponse = serde_json::from_str(&status_json)?; 
+
+        if status.status == 2001{
             println!("diawi upload not complete, trying agian after {} seconds", pool_wait_time);
             sleep(Duration::from_secs(pool_wait_time));
             continue;
         }
-        else if  status == 2000 {
+        else if  status.status == 2000 {
             return  Ok(
                 UploadResponse{
-                    link : status_body["link"].as_str().expect("failed to get download link").to_string(),
-                    qr_code: status_body["qrcode"].as_str().expect("failed to get qr code").to_string()
+                    link : status.link.expect("link not recieved in status response"), 
+                    qr_code: status.qrcode.expect("qr code not recieved in status response")
                 }
             );
         }else{
-            return Err(format!("error while uploading to diawi: {}", status_body["message"].as_str().unwrap_or("un-expected error")).into());
+            return Err(format!("error while uploading to diawi: {}", status.message.unwrap_or("un-expected error".to_string())).into());
         }
     }
 
